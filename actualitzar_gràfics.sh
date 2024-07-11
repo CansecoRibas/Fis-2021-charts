97c15f692e5b2493dee133f4baff7a241cd394ad
Joan <joan.canseco@gmail.com>
2024-07-11 13:05:53 +0200

    Creació file per automatitzar actualització

---
+++ actualitzar_gràfics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ruta al directori del projecte.
+PROJECT_DIR="C:/Users/Jcanseco/Documents/Joan/Programació i estadística/Visual Studio Code"
+
+# Canviar al directori del projecte.
+cd "$PROJECT_DIR"
+
+# Executar el script de R per generar i guardar els gràfics a la carpeta "output_gràfics".
+Rscript codi_generar_gràfics_gantt.R
+
+# Fer el commit de l'actualització.
+git commit -m "Actualització gràfics dels pacients en data $(date +'%Y-%m-%d %H:%M:%S')"
+
+# Enviar els canvis al repositori remot.
+git push origin main
